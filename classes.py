4ab28c32193b30db638242292ba0532d0f31a154
Matthieu Denoux <matthieu.denoux@exotecsolutions.com>
2016-05-04 11:18:10 +0200

    Makes the main system.

--- classes.py
+++ classes.py
@@ -2,33 +2,46 @@
 # -*-coding:utf-8 -*
 
 class Common:
-    def __init__(self, name, _type, typeName):
+    def __init__(self, key, name, _type, type_name):
+        self.key = key
         self.name = name
         self.type = _type
-        self.typeName = typeName
+        self.type_name = type_name
 
     def __str__(self):
-        return "%s %s" % (self.typeName, self.name)
+        return "%s %s" % (self.type_name, self.name)
 
     def __repr__(self):
-        return "%s %s" % (self.typeName, self.name)
+        return "%s %s" % (self.type_name, self.name)
 
     def __unicode__(self):
-        return "%s %s" % (self.typeName, self.name)
+        return "%s %s" % (self.type_name, self.name)
 
 class Parti(Common):
-    def __init__(self, name):
-        Common.__init__(self, name, "parti", "Parti")
+    def __init__(self, key, name):
+        Common.__init__(self, key, name, "parti", "Parti")
+        self.candidates = []
+
+    def add_candidate(self, candidate):
+        self.candidates.append(candidate)
 
 class Candidate(Common):
-    def __init__(self, name, parti=None):
-        Common.__init__(self, name, "candidate", "Candidate")
-        self.parti = parti
+    def __init__(self, key, partis, name, parti=None):
+        Common.__init__(self, key, name, "candidate", "Candidate")
+        self.parti = partis[parti]
+        self.parti.add_candidate(self)
+
+class Vote:
+    def __init__(self, candidates):
+        self.nb_votes = { key: 0 for key,_ in candidates.items() }
+
+    def add_vote(self, candidate):
+        self.nb_votes[candidate] += 1
 
 class Voter:
     def __init__(self, index, penchant):
         self.index = index
         self.penchant = penchant
-
-    def vote(self, votingSystem):
-        pass
+        
+    def vote(self, voting_system):
+        voting_system.add_vote(self.penchant.key)
--- simulation.py
+++ simulation.py
@@ -3,9 +3,30 @@
 
 from classes import *
 from data import *
+import random
 
-partis = {key:Parti(*parti) for key, parti in partisData.items()}
-candidates = {key:Candidate(*candidate) for key, candidate in candidatesData.items()}
+partis = {key:Parti(key, *parti) for key, parti in partisData.items()}
+candidates = {key:Candidate(key, partis, *candidate) for key, candidate in candidatesData.items()}
 
-print partis
-print candidates
+print "Partis: "
+print "\t * " + "\n\t * ".join(partis.keys())
+print "Candidates: "
+print "\t * " + "\n\t * ".join(candidates.keys())
+
+vote = Vote(candidates)
+
+for i in range(1000):
+    id_candidate = candidates.keys()[int(random.random() * len(candidates))]
+    voter = Voter(i, candidates[id_candidate])
+    voter.vote(vote)
+
+print "Votes: "
+max_votes = 0
+winner = None
+for k, nb_votes in vote.nb_votes.items():
+    print "\t * " + k + ": " + str(nb_votes)
+    if nb_votes > max_votes:
+        max_votes = nb_votes
+        winner = k
+
+print "\nVictory of " + winner
